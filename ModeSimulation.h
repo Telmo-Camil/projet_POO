d9e06042d3378b5ddceb549f595372b2402f5f61
RockWeb250 <zoe.truongcanh@viacesi.fr>
2024-12-03 18:22:25 +0100

    private

--- ModeSimulation.h
+++ ModeSimulation.h
@@ -8,17 +8,16 @@ class ModeSimulation {
 private:
     bool modeGraphique; // true : mode graphique, false : mode console
     int maxIterations;
+    static ModeSimulation *instance;
 
     ModeSimulation(bool graphique, int iterations);
+
+    void lancerConsole(Grille &grille, const std::string &outputPath);
+    void lancerGraphique(Grille &grille);
 
 public:
     static ModeSimulation *getInstance(bool graphique, int iterations);
 
     void lancer(Grille &grille, const std::string &outputPath = "");
-
-private:
-    static ModeSimulation *instance;
-
-    void lancerConsole(Grille &grille, const std::string &outputPath);
-    void lancerGraphique(Grille &grille);
 };
+
