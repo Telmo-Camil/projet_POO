39828bf76d419af9231f49ac8c462ffe62a5a59e
RockWeb250 <zoe.truongcanh@viacesi.fr>
2024-12-03 11:35:30 +0100

    const base

--- Constants.cpp
+++ Constants.cpp
@@ -1,6 +1,6 @@
 #include "Constants.h"
 
-const int cellSize = 20;    // Taille des cellules pour un meilleur affichage
-const int gridWidth = 10;   // Largeur définie dans le fichier (nombre de colonnes)
-const int gridHeight = 5;   // Hauteur définie dans le fichier (nombre de lignes)
+const int cellSize = 10;    // Taille des cellules pour un meilleur affichage
+const int gridWidth = 80;   // Largeur définie dans le fichier (nombre de colonnes)
+const int gridHeight = 80;   // Hauteur définie dans le fichier (nombre de lignes)
 
