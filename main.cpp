308a55eb4c430a0fd21995c76fc4d3e95f4f081a
RockWeb250 <zoe.truongcanh@viacesi.fr>
2024-12-03 10:17:59 +0100

    nv main

--- main.cpp
+++ main.cpp
@@ -1,35 +1,35 @@
 #include "Grille.h"
 #include "Constants.h"
+#include "Interface.h" // Inclusion pour la fonction renderGrid
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
 using namespace std; 
-using namespace sf; //regroupe toutes les classes, fonctions, et types de la bibliothèque SFML
+using namespace sf; // Regroupe toutes les classes, fonctions, et types de la bibliothèque SFML
 
 int main() {
     Grille grille(gridWidth, gridHeight);
 
     try {
         grille.chargerDepuisFichier("etat_initial.txt");
-    } catch (const std::exception &e) {
-        std::cerr << "Erreur : " << e.what() << std::endl;
+    } catch (const exception &e) {
+        cerr << "Erreur : " << e.what() << std::endl;
         return 1;
     }
 
-    sf::RenderWindow window(sf::VideoMode(gridWidth * cellSize, gridHeight * cellSize), "Game of Life");
+    RenderWindow window(sf::VideoMode(gridWidth * cellSize, gridHeight * cellSize), "Game of Life");
 
     while (window.isOpen()) {
-        sf::Event event;
+        Event event;
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed)
                 window.close();
         }
 
         grille.mettreAJour();         // Actualise l'état de la grille
         renderGrid(window, grille);  // Affiche la grille mise à jour
-        sf::sleep(sf::milliseconds(100)); // Pause entre les itérations
+        sleep(milliseconds(100)); // Pause entre les itérations
     }
 
     return 0;
 }
-
